57333e2ad3570bc56b5b8f419718e780a8982383
TylerT <alpinetyler@gmail.com>
2019-05-10 16:06:01 -0600

    Complete

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,10 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  var sentence = ''
+  for(var key in obj)
+   sentence += obj[key]
+   return sentence 
 }
 
 
@@ -54,7 +57,13 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+   for(var key in obj)
+   if(obj[key]> 10){
+     obj[key] = 0
+   }
+   return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +75,11 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){  
+for(var key in obj){
+  obj[key] *= 2
+  }return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -81,7 +94,15 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function secrets(obj){
+  var str = ''
+  for(var key in obj){
+    if (key.substring(0,2) == 'sh' ){
+      str += obj[key]
+  
+    }
+  }return str
+}
 
 /* 
   Sometimes it's needed to delete object properties. 
@@ -112,7 +133,13 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function removePassword(obj){
+  for(var key in obj){
+    if(key === 'password'){
+      delete obj[key]
+    }
+  }return obj
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -130,7 +157,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(var key in deleteTheBigNumbers){
+  if (deleteTheBigNumbers[key] > 100){
+  delete deleteTheBigNumbers[key]
+}
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,7 +174,13 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function startsWithK(obj){
+  for(var key in obj){
+    if (key.substring(0,1) === 'k'){
+      delete obj[key]
+    }
+  }return obj
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -159,4 +196,9 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
-
+function hiddenTreasure(obj){
+  for(var key in obj){
+    if(!obj[key].includes('treasure'))
+     delete obj[key]
+  }return obj
+}
