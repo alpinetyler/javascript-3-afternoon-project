c1e6da95b163c0c0bb8229156dbbe0a9298ef15e
TylerT <alpinetyler@gmail.com>
2019-05-11 18:49:27 -0600

    Complete!

--- spread.js
+++ spread.js
@@ -18,7 +18,7 @@ let product = {
 }
 
 // code here
-
+let productCopy = {...product}
 
 
 
@@ -30,6 +30,7 @@ let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
 
+let colorsCopy = [...colors]
 
 
 
@@ -44,7 +45,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+var result = addNums(...numbers )
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +55,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+var combinedArrays = [...arr1, ...arr2]
